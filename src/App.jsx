e8df1721cc235d0330cdc20e981e450f29e8fc34
pedro roca de togores <pedrobgrdt@gmail.com>
2024-02-08 18:19:27 +0100

    react-form-creation-trial

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,82 @@ import StudentCard from "./components/StudentCard";
 import studentsData from "./assets/students.json";
 
 function App() {
+
   const [students, setStudents] = useState(studentsData);
+
+  const addNewStudent = newStudent => {
+    const studentCopy = [...students]          // Dado que unshift NO RETORNA UN NUEVO ARRAY es necesario sacar copia       
+    studentCopy.unshift(newStudent)            // Manipular copia (no usar retorno, no retorna un nuevo array, NOOOO)
+    setStudents(studentCopy)
+
+  }
+
+  const [fullName, setFullname] = useState("")
+  const [image, setImage] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+  const [program, setProgram] = useState("")
+  const [graduationYear, setGraduationYear] = useState(2023)
+  const [graduated, setGraduated] = useState(false)
+
+  const handleFullNameChange = event => setFullname(event.target.value)
+  const handleImageChange = event => setImage(event.target.value)
+  const handlePhoneChange = event => setPhone(event.target.value)
+  const handleEmailChange = event => setEmail(event.target.value)
+  const handleProgramChange = event => setProgram(event.target.value)
+  const handleGraduationYearChange = event => setGraduationYear(event.target.value)
+  const handleGraduationCheck = event => setGraduated(event.target.checked)
+
+  const handleSubmitChanger = event => {
+
+    event.preventDefault()
+
+    const newStudent = {
+      fullName: fullName,
+      email: email,
+      phone: phone,
+      program: program,
+      image: image,
+      graduationYear: graduationYear,
+      graduated: graduated,
+    }
+    addNewStudent(newStudent)
+  }
 
 
   return (
-    <div className="App pt-20">
+    <div className="App pt-20"   >
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmitChanger} >
         <span>Add a Student</span>
         <div>
-          <label>
+          <label >
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input name="fullName" type="text" placeholder="Full Name" value={fullName} onChange={handleFullNameChange} />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input name="image" type="url" placeholder="Profile Image" value={image} onChange={handleImageChange} />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input name="phone" type="tel" placeholder="Phone" value={phone} onChange={handlePhoneChange} />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input name="email" type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select name="program" value={program} onChange={handleProgramChange}  >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -56,6 +96,8 @@ function App() {
               name="graduationYear"
               type="number"
               placeholder="Graduation Year"
+              value={graduationYear}
+              onChange={handleGraduationYearChange}
               minLength={4}
               maxLength={4}
               min={2023}
@@ -65,10 +107,10 @@ function App() {
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input name="graduated" type="checkbox" checked={graduated} onChange={handleGraduationCheck} />
           </label>
 
-          <button type="submit">Add Student</button>
+          <button type="submit" onSubmit={handleSubmitChanger}>Add Student</button>
         </div>
 
       </form>
@@ -88,4 +130,8 @@ function App() {
   );
 }
 
+
 export default App;
+
+
+// addNewStudent = { addNewStudent }
