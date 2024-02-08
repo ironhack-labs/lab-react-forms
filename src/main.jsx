480045f2042c3eca432d02224fa15fd7a808867e
chyVacheck <dmytro.shah.work@gmail.com>
2024-02-08 10:45:27 +0100

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
+// ! modules
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// ? styles
 import "./index.css";
-import App from "./App";
+
+// App
+import App from "./App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
