434819c0a5a8a79f75cd941d5efeddd94e680809
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-11 22:38:44 +0100

    Add ids to the corresponding inputs

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState([]);
-  
+
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
@@ -23,97 +23,105 @@ const handleSubmit = (e)=> {
 };
 
 
-  return (
-    <div className="App pt-20">
-      <Navbar />
+
+return (
+  <div className="App pt-20">
+    <Navbar />
 
       {/* FORM */}
       <form onSubmit={handleSubmit}>
-      
-        <input
-          type="text"
-          name="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
 
-          placeholder="First Name"
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={lastName}
+<input
+  type="text"
+  id="firstName"
+  name="firstName"
+  value={firstName}
+  onChange={(e) => setFirstName(e.target.value)}
 
-
-          onChange={(e) => setLastName(e.target.value)}
-          placeholder="Last Name"
-        />
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <input
-          type="tel"
-          name="phoneNumber"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-
-          placeholder="Phone Number"
-
-        />
-        <input
-          type="date"
-          name="dateOfBirth"
-          value={dateOfBirth}
-          onChange={(e) => setDateOfBirth(e.target.value)}
-
-          placeholder="Date of Birth"
-        />
-        <select
-          name="gender"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        >
-          <option value="">Select Gender</option>
-          <option value="male">Male</option>
-
-          <option value="female">Female</option>
-          
-          <option value="other">Other</option>
-        </select>
-        <input
-          type="checkbox"
-          name="isGraduated"
-          checked={isGraduated}
-          onChange={(e) => setIsGraduated(e.target.checked)}
-        />
-        <label htmlFor="isGraduated">Is Graduated?</label>
-        <input
-          type="number"
-
-          name="gpa"
-
-          value={gpa}
-          onChange={(e) => setGpa(parseFloat(e.target.value))}
-          placeholder="GPA"
-          min="0" 
-          max="4.0" 
-
-          step="0.01" 
-        />
-       
-        <button type="submit">Add Student</button>
-      </form>
-     
+  placeholder="First Name"
+/>
+<input
+  type="text"
+  id="lastName"
+  name="lastName"
+  value={lastName}
 
 
+  onChange={(e) => setLastName(e.target.value)}
+  placeholder="Last Name"
+/>
+<input
+  type="email"
+  id="email"
+  name="email"
+  value={email}
+  onChange={(e) => setEmail(e.target.value)}
+  placeholder="Email"
+/>
+<input
+  type="tel"
+  id="phoneNumber"
+  name="phoneNumber"
+  value={phoneNumber}
+  onChange={(e) => setPhoneNumber(e.target.value)}
 
-      <TableHeader />
+  placeholder="Phone Number"
+
+/>
+<input
+  type="date"
+  id="dateOfBirth"
+  name="dateOfBirth"
+  value={dateOfBirth}
+  onChange={(e) => setDateOfBirth(e.target.value)}
+
+  placeholder="Date of Birth"
+/>
+<select
+  name="gender"
+  id="gender"
+  value={gender}
+  onChange={(e) => setGender(e.target.value)}
+>
+  <option value="">Select Gender</option>
+  <option value="male">Male</option>
+
+  <option value="female">Female</option>
+
+  <option value="other">Other</option>
+</select>
+<input
+  type="checkbox"
+  name="isGraduated"
+  checked={isGraduated}
+  onChange={(e) => setIsGraduated(e.target.checked)}
+/>
+<label htmlFor="isGraduated">Is Graduated?</label>
+<input
+  type="number"
+
+  name="gpa"
+
+  value={gpa}
+  onChange={(e) => setGpa(parseFloat(e.target.value))}
+  placeholder="GPA"
+  min="0" 
+  max="4.0" 
+
+  step="0.01" 
+/>
+
+<button type="submit">Add Student</button>
+</form>
+      {/* FORM END */}
+
+ {/* TABLE/LIST HEADER */}
+ <TableHeader />
+
+    
 
 
-      {students &&
+ {students &&
         students.map((student) => {
           return <StudentCard key={student.email} {...student} />;
         })}
