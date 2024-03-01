4d80b0be7a3513e0c888eccb5b6b127fa08a4d55
Jonathan <nathan.diaz062@gmail.com>
2024-03-01 16:45:52 -0400

    Solve Lab

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,66 @@ import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  // const [studentsData, setStudentsData] = useState(studentsData);
+  //variables:
+
+  const [fullName, setFullName] = useState(""); 
+  const [email, setEmail] = useState(""); 
+  const [phone, setPhone] = useState(""); 
+  const [program, setProgram] = useState(""); 
+  const [image, setImage] = useState(""); 
+  const [graduationYear, setGraduationYear] = useState(2023); 
+  const [graduated, setGraduated] = useState(false); 
+
+
+  //handle functions:
+  const handleFullNameInput = e => setFullName(e.target.value);
+  const handleEmailInput = e => setEmail(e.target.value);
+  const handlePhoneInput = e => setPhone(e.target.value);
+  const handleProgramInput = e => setProgram(e.target.value);
+  const handleImageInput = e => setImage(e.target.value);
+  const handleGraduationYearInput = e => setGraduationYear(e.target.value);
+  const handleGraduatedInput = e => setGraduated(e.target.checked);
+
+
+
+
+  const addStudent = (newStudent) => {
+    // Create a new array
+    const updatedStudents = [...students, newStudent];
+    const updatedStudentsData = [...studentsData, newStudent]; // <== ADD
+  
+    setStudents(updatedStudents);
+    setStudentsData(updatedStudentsData);   // <== ADD
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newStudent = { 
+      fullName,
+      email,
+      phone,
+      program,
+      image,
+      graduationYear,
+      graduated
+    };
+ 
+    console.log("Submitted", newStudent);
+    addStudent(newStudent);          // <== ADD
+ 
+    // Reset the state
+    setFullName("");
+    setEmail("");
+    setPhone("");
+    setProgram("");
+    setImage("");
+    setGraduationYear(2023);
+    setGraduated(false);
+
+}
+
+
 
 
   return (
@@ -15,34 +75,34 @@ function App() {
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input name="fullName" type="text" placeholder="Full Name" value = {fullName} onChange={handleFullNameInput }/>
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input name="image" type="url" placeholder="Profile Image" value = {image} onChange={handleImageInput}/>
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input name="phone" type="tel" placeholder="Phone" value = {phone} onChange={handlePhoneInput}/>
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input name="email" type="email" placeholder="Email" value = {email} onChange={handleEmailInput}/>
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select name="program" value = {program} onChange={handleProgramInput}>
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -60,12 +120,14 @@ function App() {
               maxLength={4}
               min={2023}
               max={2030}
+              value = {graduationYear}
+              onChange={handleGraduationYearInput}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input name="graduated" type="checkbox" checked = {graduated} onChange={handleGraduatedInput}/>
           </label>
 
           <button type="submit">Add Student</button>
