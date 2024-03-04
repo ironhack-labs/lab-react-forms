08bd1b1ed99651206a5295274799a665e35b1d55
Dalton <daltongetbaker@gmail.com>
2024-03-04 10:34:37 -0500

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,17 @@ import TableHeader from "./components/TableHeader";
 import StudentCard from "./components/StudentCard";
 
 import studentsData from "./assets/students.json";
+import AddStudent from "./components/AddStudent";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+
+  const handleSubmit = (newStudent) => {
+
+    let newStudents = [newStudent, ...students]
+    setStudents(newStudents)
+    
+  }
 
 
   return (
@@ -15,63 +23,7 @@ function App() {
       <Navbar />
 
       {/* FORM */}
-      <form>
-        <span>Add a Student</span>
-        <div>
-          <label>
-            Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
-          </label>
-
-          <label>
-            Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
-          </label>
-
-          <label>
-            Phone
-            <input name="phone" type="tel" placeholder="Phone" />
-          </label>
-
-          <label>
-            Email
-            <input name="email" type="email" placeholder="Email" />
-          </label>
-        </div>
-
-        <div>
-          <label>
-            Program
-            <select name="program">
-              <option value="">-- None --</option>
-              <option value="Web Dev">Web Dev</option>
-              <option value="UXUI">UXUI</option>
-              <option value="Data">Data</option>
-            </select>
-          </label>
-
-          <label>
-            Graduation Year
-            <input
-              name="graduationYear"
-              type="number"
-              placeholder="Graduation Year"
-              minLength={4}
-              maxLength={4}
-              min={2023}
-              max={2030}
-            />
-          </label>
-
-          <label>
-            Graduated
-            <input name="graduated" type="checkbox" />
-          </label>
-
-          <button type="submit">Add Student</button>
-        </div>
-
-      </form>
+      <AddStudent handleSubmit={handleSubmit} />
       {/* FORM END */}
 
 
