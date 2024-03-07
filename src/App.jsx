e42e750abbb8116277237c6e8c3fd89dc8238b5f
Memoryman2022 <rlacannon@gmail.com>
2024-03-07 15:00:35 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,108 @@ import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  const [fullName, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [program, setProgram] = useState("");
+  const [gradYear, setGradYear] = useState("--NONE--");
+  const [graduated, setGraduated] = useState(false);
 
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    const newStudent = {
+      fullName,
+      image,
+      phoneNumber,
+      email,
+      program,
+      gradYear,
+      graduated,
+    };
+    setStudents([...students, newStudent]);
+    setName("");
+    setImage("");
+    setPhoneNumber("");
+    setEmail("");
+    setProgram("");
+    setGradYear("--NONE--");
+    setGraduated(false);
+  };
+  // const FormSubmission = ({ students, setStudents }) => {
+  //   const [fullName, setName] = useState("");
+  //   const [image, setImage] = useState("");
+  //   const [phoneNumber, setPhoneNumber] = useState("");
+  //   const [email, setEmail] = useState("");
+  //   const [program, setProgram] = useState("");
+  //   const [gradYear, setGradYear] = useState("--NONE--");
+  //   const [graduated, setGraduated] = useState(false);
+  // };
+  // const handleFullNameInput = (e) => setName(e.target.value);
+  // const handleImageInput = (e) => setImage(e.target.value);
+  // const handlePhoneNumberInput = (e) => setPhoneNumber(e.target.value);
+  // const handleEmailInput = (e) => setEmail(e.target.value);
+  // const handleProgramInput = (e) => setProgram(e.target.value);
+  // const handleGradYearInput = (e) => setGradYear(e.target.value);
+  // const handleGraduatedInput = (e) => setGraduated(e.target.value);
 
   return (
     <div className="App pt-20">
       <Navbar />
-
       {/* FORM */}
-      <form>
+      <form className="student-form" onSubmit={handleFormSubmission}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              value={fullName}
+              type="text"
+              placeholder="Full Name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              value={image}
+              type="url"
+              placeholder="Profile Image"
+              onChange={(event) => setImage(event.target.value)}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              value={phoneNumber}
+              type="tel"
+              placeholder="Phone"
+              onChange={(event) => setPhoneNumber(event.target.value)}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              value={email}
+              type="email"
+              placeholder="Email"
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select
+              value={program}
+              onChange={(event) => setProgram(event.target.value)}
+            >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -53,32 +120,32 @@ function App() {
           <label>
             Graduation Year
             <input
-              name="graduationYear"
+              value={gradYear}
               type="number"
               placeholder="Graduation Year"
               minLength={4}
               maxLength={4}
               min={2023}
               max={2030}
+              onChange={(event) => setGradYear(event.target.value)}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              checked={graduated}
+              type="checkbox"
+              onChange={(event) => setGraduated(event.target.checked)}
+            />
           </label>
 
           <button type="submit">Add Student</button>
         </div>
-
       </form>
       {/* FORM END */}
-
-
       {/* TABLE/LIST HEADER */}
       <TableHeader />
-
-
       {/* STUDENT LIST */}
       {students &&
         students.map((student) => {
@@ -87,5 +154,5 @@ function App() {
     </div>
   );
 }
-
+// }
 export default App;
